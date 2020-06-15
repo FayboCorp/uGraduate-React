a354d64fe3c6f8037b9cdd5403fb0dbb5b32bf8c
FayboCorp <32463786+FayboCorp@users.noreply.github.com>
2020-06-15 04:21:41 -0500

    Added Login Component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,31 @@ import React, {Component} from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home'
 import Scheduler from './Components/Scheduler/Scheduler';
+import Login from './Components/Login/Login';
 import './App.css';
 
 // TODO: Work on getting these REST requests!
 
 class app extends Component{
     state = {
-        showHome: true,
+        showLogin: true,
+        showHome: false,
         showScheduler: false,
         showAdvisor: false,
     };
 
 
     dynamicDisplay = (props) => {
-        this.setState({showHome: false, showScheduler: false, showAdvisor: false});
+        this.setState({showLogin: false, showHome: false, showScheduler: false, showAdvisor: false});
 
         if(props === 1){
             this.setState({showHome: true});
         }
         else if(props === 2){
             this.setState({showScheduler: true});
+        }
+        else if(props === -1){
+            this.setState({showLogin: true});
         }
         else {
             this.setState({showHome: true});
@@ -37,6 +42,9 @@ class app extends Component{
         }
         else if (this.state.showScheduler){
             cockpit = (<Scheduler/>);
+        }
+        else if(this.state.showLogin){
+            cockpit = (<Login/>);
         }
         else {
             cockpit = null;
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class Login extends Component{
+
+    render(){
+
+
+
+        return(
+            <div className="cockpit">
+
+            </div>
+        );
+    }
+
+}
+
+export default Login;
